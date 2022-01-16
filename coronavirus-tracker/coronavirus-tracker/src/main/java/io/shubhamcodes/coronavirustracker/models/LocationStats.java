981b3754b0d6f32f93c525dc9f52f30d410fe81b
Shubham <shubhampawar503@gmail.com>
2022-01-16 19:05:05 +0530

    Added model package and location stats

---
+++ coronavirus-tracker/coronavirus-tracker/src/main/java/io/shubhamcodes/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,21 @@
+package io.shubhamcodes.coronavirustracker.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocationStats {
+ @Override
+	public String toString() {
+		return "LocationStats [state=" + state + ", country=" + country + ", latestTotalCases=" + latestTotalCases
+				+ "]";
+	}
+private String state;
+ private String country;
+ private int latestTotalCases;
+}
