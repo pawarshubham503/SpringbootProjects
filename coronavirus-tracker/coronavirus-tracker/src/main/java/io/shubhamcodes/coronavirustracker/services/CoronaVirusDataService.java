6c3e0cee75596b130924bbf86a08140267af14b4
Shubham <shubhampawar503@gmail.com>
2022-01-16 18:46:22 +0530

    added csv parsing

--- coronavirus-tracker/coronavirus-tracker/src/main/java/io/shubhamcodes/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/coronavirus-tracker/src/main/java/io/shubhamcodes/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,6 +1,9 @@
 package io.shubhamcodes.coronavirustracker.services;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,6 +11,9 @@ import java.net.http.HttpResponse;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +21,20 @@ public class CoronaVirusDataService {
 	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
 
 	@PostConstruct
+	@Scheduled(cron="* * * * * *")
 	public void fetchVirusData() throws IOException, InterruptedException {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
 
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-		System.out.println(httpResponse.body());
+		StringReader csvBodyReader = new StringReader(httpResponse.body());
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+		for (CSVRecord record : records) {
+		    String state = record.get("Province_State");
+		   System.out.println(state);
+		  
+		}
+		  
 	}
 }
